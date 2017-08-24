53cb278c244ea095b1a4048a03a1f4ee5a601137
hannahbull <31089253+hannahbull@users.noreply.github.com>
2017-08-25 03:20:04 +1000

    Update F_group_mean_var.R

--- Functions/F_group_mean_var.R
+++ Functions/F_group_mean_var.R
@@ -3,9 +3,7 @@ group_mean_var <- function(donnees, x) {
   group_allocation <-c(1:20)
   
   groupchar <-data.frame(group_allocation)
-  
- # groupchar$group_effects <- rnorm(20,0,1)
-  
+    
   mean_group_allocation <- aggregate(donnees$x, list(group_allocation=donnees$group_allocation), mean)
   colnames(mean_group_allocation)[2] <- "mean_group_allocation"
   
@@ -17,11 +15,7 @@ group_mean_var <- function(donnees, x) {
   
   donnees <- merge(donnees, groupchar, by="group_allocation")
   
-  # plot(groupchar$mean_group_allocation, groupchar$var_group_allocation, main=paste0("Mean vs Variance",s),
-  #      xlab="mean", ylab="variance", pch=19)
-  
-  
-  #compute leave-out means
+  #compute leave-out means and variances
   
   leave_out_mean <- function(mean,x) {
     result <- (20*mean-x)/(20-1)
